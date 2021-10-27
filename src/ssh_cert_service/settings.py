e67259ee2cefd57edf4b8bbcee1e37dfdbb41bdb
jeff <jeff.castrillon@studionone.com.au>
2021-10-27 14:11:48 +1000

    addressing comments

--- src/ssh_cert_service/settings.py
+++ src/ssh_cert_service/settings.py
@@ -6,5 +6,5 @@
 import os
 
 # get path from the masterkey is located 
-MASTER_PRIVATE_KEY_PATH = os.environ.get("COESRA_PRIVATE_KEY_PATH_ENV") 
+MASTER_PRIVATE_KEY_PATH = os.environ["COESRA_PRIVATE_KEY_PATH_ENV"] 
 MASTER_KEY_PASSPHRASE = os.environ.get("MASTER_KEY_PASSPHRASE_ENV", "") 
