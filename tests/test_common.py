b059f3f4882e9d4a9c92b6ff6a4480edf73cbd73
Gerhard Weis <g.weis@uq.edu.au>
2021-11-18 07:19:13 +1000

    fix test code for validity

--- tests/test_common.py
+++ tests/test_common.py
@@ -1,24 +1,29 @@
-from ssh_cert_service.utils.common import validity_data
+import pytest
 
 VALIDITY = "+1d"
 MIN_VALIDITY = "always"
 
-def test_validity_data():
-    validity = validity_data(VALIDITY, MIN_VALIDITY, "1d")
+testdata = [
+    # validity, min, max, expected result
+    ("+1d", MIN_VALIDITY, "1d", "always:+1d"),
+    ("+6m", MIN_VALIDITY, "1d", "always:+6m"),
+    ("+6m", MIN_VALIDITY, "66m", "always:+6m"),
+    # FIXME: +1d < 3m .... should not fail
+    ("+1d", MIN_VALIDITY, "3m", "always:+1d"),
+    # FIXME: validity and max should use the same format
+    ("+3d", MIN_VALIDITY, "+1m", "always:+3d"),
+    # FIXME: API allows passing min validity let's see what happens
+    #        which format to use value or max?
+    ("+1d", "2d", "2d", "-2d:+1d"),
+    ("+1d", "-2d", "2d", "-2d:+1d"),
+]
+
+
+@pytest.mark.parametrize("value, min, max, expected", testdata)
+def test_validity_data(value, min, max, expected):
+    from ssh_cert_service.utils.common import validity_data
+
+    validity = validity_data(value, min, max)
 
     assert validity is not None
-    assert validity == f"{MIN_VALIDITY}:{VALIDITY}"
-
-def test_fail_validity_data():
-    value = "+6m"
-    validity = validity_data(value, MIN_VALIDITY, "1d")
-
-    assert validity is not None
-    assert validity != f"{MIN_VALIDITY}:{value}"
-
-def test_different_validity_data():
-    value = "+6m"
-    validity = validity_data(value, MIN_VALIDITY, "66m")
-
-    assert validity is not None
-    assert validity == f"{MIN_VALIDITY}:{value}"
+    assert validity == expected
