6d9b37d7a01591d25e478e49c54803020fb24725
jeff <jeff.castrillon@studionone.com.au>
2021-11-18 16:27:11 +1000

    removing some useless code

--- src/ssh_cert_service/utils/common.py
+++ src/ssh_cert_service/utils/common.py
@@ -12,18 +12,15 @@ def validity_data(validity_request: int, validity_start: int, validity_end: int)
     if not isinstance(validity_request, int):
         raise Exception("The validity request is not correct please check your inputs")
 
+    output_to = f"+{validity_end}s"
+    output_from = "always"
+
     if not validity_end or validity_request <= validity_end:
         # If the settings are empty it means forever
         output_to = f"+{validity_request}s" if validity_request else "forever"
-    # If the validity request fails then work with the settings values
-    else:
-        output_to = f"+{validity_end}s"
 
     # Instace the current time plus the validity_start
     if validity_start:
         output_from = f"-{validity_start}s"
-    # If the settings are empty it means always
-    else:
-        output_from = "always"
 
     return f"{output_from}:{output_to}"
