7344d46e80e4b55e39a83237af5ff5f9e73cfb11
jeff <jeff.castrillon@studionone.com.au>
2021-11-18 15:34:16 +1000

    improving test

--- src/ssh_cert_service/utils/common.py
+++ src/ssh_cert_service/utils/common.py
@@ -1,56 +1,44 @@
 import re
+from datetime import timedelta, datetime
 
-def validity_data(validity: str, min: str, max: str) -> str:
+
+def validity_data(validity_request: int, validity_start: int, validity_end: int) -> str:
     """
-    Check the validity data and return the correct values
+    Check the validity time that the certificate will exprire
 
     :return: str with the correct validity strcuture
     """
 
-    # System should define acceptable values such as 'minutes, hours, days, weeks'
-    #       By default if something fails it will take the first of the list
-    r_compare = r'([\+|\-])(\d+)([mhdw]+)'
+    if not isinstance(validity_request, int):
+        raise Exception("The validity request is not correct please check your inputs")
 
-    # Check that the defined min is correct
-    if min != 'always':
-        min_r = re.search(r_compare, min, re.IGNORECASE)
-        if not min_r:
-            raise Exception("The Min configuration is incorrect pleas check value again")
-        min = min_r.group(1) + min_r.group(2) + min_r.group(3)[0]
+    current_time = datetime.today()
+    total_minutes = 1
+    hour_in_min = 60
+    one_min_in_sec = 60
 
-    # Check that the defined max is correct
-    if max != 'forever':
-        max_r = re.search(r_compare, max, re.IGNORECASE)
-        if not max_r:
-            raise Exception("The Max configuration is incorrect pleas check value again")
-        max = max_r.group(1) + max_r.group(2) + max_r.group(3)[0]
+    # Work with timedelta and add the validity_request in seconds
+    validity_request = timedelta(seconds=validity_request)
+
+    if validity_request.total_seconds() <= validity_end:
+        # Get the total of seconds between the difference of the current time and future time
+        seconds = int(((current_time + validity_request) - current_time).total_seconds())
+        total_minutes = int(seconds / hour_in_min) if seconds >= one_min_in_sec else total_minutes
+        output_to = f"+{total_minutes}m"
+    # If the validity request fails then work with the settings values
+    elif validity_end:
+        output_to = int(validity_end / hour_in_min) if validity_end >= one_min_in_sec else total_minutes
+        output_to = f"+{output_to}m"
+    # If the settings are empty it means forever
     else:
-        # If settings max is forever the is not need to check anything else
-        return f"{min}:{max}"
+        output_to = "forever"
 
-    # Instance default output
-    default = f"{min}:{max}"
+    # Instace the current time plus the validity_start
+    if validity_start:
+        output_from = int(validity_start / hour_in_min) if validity_start >= one_min_in_sec else total_minutes
+        output_from = f"-{output_from}m"
+    # If the settings are empty it means always
+    else:
+        output_from = "always"
 
-    if not validity:
-        return default
-
-    # Match regex for the validity structure -1d:+1d
-    regex = r'\+(\d+)([dwmh])'
-    result = re.search(regex, validity, re.IGNORECASE)
-
-    # If the regex does not match the two (int & str) values return default
-    if not result or len(result.groups()) != 2:
-        return default
-
-    sys_value = int(max_r.group(2))
-    sys_len = [char for char in max_r.group(3)]
-
-    max_value = int(result.group(1))
-    max_len = result.group(2)
-
-    # Compare the coming data with the defined max validity in the system
-    if max_len not in sys_len or max_value > sys_value:
-        return default
-
-    # If nothing fails return the request validity
-    return f"{min}:{validity}"
+    return f"{output_from}:{output_to}"
--- tests/test_common.py
+++ tests/test_common.py
@@ -1,25 +1,68 @@
 import pytest
+from datetime import timedelta
 
 VALIDITY = "+1d"
-MIN_VALIDITY = "always"
+MIN_VALIDITY = 0
+
+def get_seconds(
+    days: int = 0,
+    seconds: int = 0,
+    minutes: int = 0,
+    hours: int = 0,
+    weeks: int = 0):
+    return int(timedelta(
+        days=days,
+        seconds=seconds,
+        minutes=minutes,
+        hours=hours,
+        weeks=weeks,
+    ).total_seconds())
+
+
+validity_settings = [
+    get_seconds(days=1, hours=2), #0
+    get_seconds(minutes=7),       #1
+    get_seconds(minutes=5),       #2
+    get_seconds(minutes=66),      #3
+    get_seconds(minutes=3),       #4
+    get_seconds(minutes=3),       #5
+    get_seconds(days=2),          #6
+    get_seconds(days=1),          #7
+]
+
+input_validity = [
+    get_seconds(days=1),    #0
+    get_seconds(minutes=6), #1
+    get_seconds(minutes=5), #2
+    get_seconds(hours=1),   #3
+    get_seconds(days=1),    #4
+    get_seconds(days=3),    #5
+    get_seconds(days=1),    #6
+    get_seconds(days=1),    #7
+    get_seconds(days=4),    #8
+]
+
 
 testdata = [
     # validity, min, max, expected result
-    ("+1d", MIN_VALIDITY, "+1d", "always:+1d"),
-    ("+6m", MIN_VALIDITY, "+7dm", "always:+6m"),
-    ("+6m", MIN_VALIDITY, "+66m", "always:+6m"),
-    # FIXME: 1h < 66m .... is 66m even allowed? or should this turn into 1h6m ?
-    ("+1h", MIN_VALIDITY, "+66mh", "always:+1h"),
-    # FIXME: +1d < 3m .... should not fail
-    ("+1d", MIN_VALIDITY, "+3md", "always:+1d"),
-    # FIXME: validity and max should use the same format
-    ("+3d", MIN_VALIDITY, "+4md", "always:+3d"),
-    # FIXME: API allows passing min validity let's see what happens
-    #        which format to use value or max?
-    ("+1d", "-2d", "+2d", "-2d:+1d"),
-    ("+1d", "-2d", "+2d", "-2d:+1d"),
-    ("+1d", "always", "forever", "always:forever"),
-    ("+4d", "-2dmhw", "+2mhdw", "-2d:+2m"),
+    # +1d, always, +1d+2h, always:+1440m
+    (input_validity[0], MIN_VALIDITY, validity_settings[0], "always:+1440m"),
+    # +6m, always, +7m, always:+6m
+    (input_validity[1], MIN_VALIDITY, validity_settings[1], "always:+6m"),
+    # +5m, always, +5m, always:+5m
+    (input_validity[2], MIN_VALIDITY, validity_settings[2], "always:+5m"),
+    # +1h, always, +66m, always:+60m
+    (input_validity[3], MIN_VALIDITY, validity_settings[3], "always:+60m"),
+    # +1d, always, +3m, always:+3m
+    (input_validity[4], MIN_VALIDITY, validity_settings[4], "always:+3m"),
+    # +3d, always, +3m, always:+3m
+    (input_validity[5], MIN_VALIDITY, validity_settings[5], "always:+3m"),
+    # +1d, -2d, +2d, -2880m:+1440m
+    (input_validity[6], validity_settings[6], validity_settings[6], "-2880m:+1440m"),
+    # +1d, always, forever, always:forever
+    (input_validity[7], get_seconds(), get_seconds(), "always:forever"),
+    # +4d, -1d, +1d, -1440m:+1440m
+    (input_validity[8], validity_settings[7], validity_settings[7], "-1440m:+1440m"),
 ]
 
 
